cbc826de71360384e6d6680950d4707ff79efcf8
Glowzer <31700869+Glowzer@users.noreply.github.com>
2017-10-02 00:05:06 +0300

    first

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,49 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      this.config = config;
+      this.initial = config.initial;
+      this.state = config.initial;
+      this.stateArray = [];
+      this.redoArray = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      this.state = state;
+      this.stateArray.push(this.state);
+      this.redoArray.pop();
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      this.stateArray.push(this.state);
+      this.redoArray.pop();
+      this.state = this.config.states[this.state].transition[event];
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.state = this.initial;
+      this.stateArray.push(this.state);
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +53,50 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+      if(event === undefined)
+        return ['sleeping','normal','hungry','busy'];
+      var array = [];
+      for(var k in this.config.states)
+          if(event in this.config.states[k].transition)
+            array.push(k);
+      return array;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if (this.stateArray.length == 0)
+            return false;
+        this.redoArray.push(this.state);
+        this.state = this.stateArray.pop();
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+      if (this.redoArray == 0)
+           return false;
+       this.stateArray.push(this.state);
+       this.state = this.redoArray.pop();
+       return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.state = this.initial;
+        this.stateArray.length = 0;
+        this.redoArray.length = 0;
+    }
 }
 
 module.exports = FSM;
